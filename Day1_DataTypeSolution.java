d1ebdbff259a62151d9f8690194ed0bbe4fd458c
chethankb007 <67217350+chethankb007@users.noreply.github.com>
2020-07-21 13:25:26 +0530

    Update Day1_DataTypeSolution.java

--- Day1_DataTypeSolution.java
+++ Day1_DataTypeSolution.java
@@ -1,35 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+	
     public static void main(String[] args) {
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
-
+		
         Scanner scan = new Scanner(System.in);
 
-        /* Declare second integer, double, and String variables. */
-        int i2;
-        double d2;
-        String s2;
-
-        /* Read and save an integer, double, and String to your variables.*/
-        i2 = scan.nextInt();
-        d2 = scan.nextDouble();
-
+        int j = scan.nextInt();
+        double e = scan.nextDouble();
         scan.nextLine();
-        s2 = scan.nextLine();
+        String t = scan.nextLine();
+        scan.close();
 
-        /* Print the sum of both integer variables on a new line. */
-        System.out.println(i + i2);
-
-        /* Print the sum of the double variables on a new line. */
-        System.out.println(d + d2);
-
-        /* Concatenate and print the String variables on a new line;
-            the 's' variable above should be printed first. */
-        System.out.println(s.concat(s2));
-
+        System.out.println(i+j);
+        System.out.println(d+e);
+        System.out.println(s+""+t);
         scan.close();
     }
 }
